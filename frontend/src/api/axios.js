2a7ebba6aebbc981e0db14768bdb958cf84bd1e4
Sufaina Nesrin <sufainanasrin2003@gmail.com>
2026-02-10 00:34:31 +0530

    env added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: API,
   withCredentials: true, 
 });
 
